3d14a4ac394987d86860aa13490962a9cbb4b2ce
DTrejo <david.trejo@voxer.com>
2012-07-09 23:25:22 -0700

    tests passing, yay

--- npmbro.js
+++ npmbro.js
@@ -1,18 +1,6 @@
-var re =
-  { search: /npm\s+search\s+((?:[a-z0-9_-]*))/
-  , install: /npm\s+install\s+((?:[a-z0-9_-]*))/
-  , docs: /npm\s+docs\s+((?:[a-z0-9_-]*))/
-  }
-var bro =
-  { re: re
-  , search: search
-  , docs: docs
-  , install: docs
-  }
-
-module.exports = bro
-
 var jerk = require('jerk')
+var npm = require('npm')
+var EventEmitter = require('events').EventEmitter
 var options =
   { server: 'irc.freenode.net'
   , nick: 'npmbro'
@@ -22,20 +10,27 @@ var options =
     ]
   }
 
-function handlers(j) {
+var re =
+  { search: /npm search ((?:[a-z0-9_ -]*))/
+  , install: /npm install ((?:[a-z0-9_ -]*))/
+  , docs: /npm docs ((?:[a-z0-9_ -]*))/
+  }
 
-  j.watch_for('soup', function(message) {
-    message.say(message.user + ': soup is good food!')
-  })
-  j.watch_for(re.search, search)
-  j.watch_for(re.install, install)
-  j.watch_for(re.docs, docs)
-}
+var bro = new EventEmitter()
+bro.re = re
+bro.search = search
+bro.docs = docs
+bro.install = docs // alias
 
-function search (m) {
-  var reply = m.user + ': you searched for ' + m.match_data[1] + '.'
-  m.say(reply)
-  return reply
+module.exports = bro
+
+function search (m, cb) {
+  // return npm.commands.search(['some', 'args'], function (er, data) {
+    // if (er) throw new Error(er.message)
+    var reply = m.user + ': you searched for ' + m.match_data[1] + '.'
+    m.say(reply)
+    return cb(null, reply)
+  // })
 }
 
 function install (m) {
@@ -47,6 +42,22 @@ function docs (m) {
   return reply
 }
 
-if (!module.parent) {
-  jerk(handlers).connect(options)
+function handlers(j) {
+  j.watch_for(re.search, search)
+  j.watch_for(re.install, install)
+  j.watch_for(re.docs, docs)
 }
+
+if (!module.parent) {
+  bro.on('load', function (npm) {
+    jerk(handlers).connect(options)
+  })
+}
+
+npm.load({}, function (er) {
+  if (er) throw new Error(er.message)
+  npm.on('log', function (m) {
+    console.log(m)
+  })
+  bro.emit('load', npm)
+})
--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-var assert = a = require('assert')
+var specify = require('specify')
+var filters = process.argv.slice(2)
 var bro = require('./npmbro')
 var re = bro.re
 var noop = function () {}
@@ -6,7 +7,6 @@ var searches =
   [ 'npm search dude'
   , 'npm search bro'
   , '   npm search bro'
-  , 'npm    search bro'
   , 'npm search    kgoiewjgwre998932nnlj'
   , 'npm search osdignsd-'
   , 'npm search 23949---_9'
@@ -14,6 +14,17 @@ var searches =
   , 'npm search hello world'
   , 'npm search lotsa white at the end    '
   ]
+/*
+npm search dude
+npm search bro
+   npm search bro
+npm search    kgoiewjgwre998932nnlj
+npm search osdignsd-
+npm search 23949---_9
+npm search 42
+npm search hello world
+npm search lotsa white at the end
+*/
 var docs = searches
   .map(function (s) {
     return s.replace('search', 'docs')
@@ -29,21 +40,28 @@ function message(s, re) {
   }
 };
 
-console.log('==search==')
-searches.forEach(function (s) {
-  a.ok(re.search.test(s))
-  var reply = bro.search(message(s, re.search))
-  console.log(reply)
-  a.ok(reply)
-});
-
-console.log('==docs==')
-docs.forEach(function (s) {
-  a.ok(re.docs.test(s));
-  var reply = bro.docs(message(s, re.docs))
-  console.log(reply)
-  a.ok(reply)
+specify('search', function (a) {
+  a.expect(searches.length * 3)
+  searches.forEach(function (s) {
+    a.ok(re.search.test(s))
+    bro.search(message(s, re.search), function (er, reply) {
+      a.ok(!er)
+      console.log(reply)
+      a.ok(reply)
+    })
+  });
 })
 
-console.log('All tests passed!')
-process.exit(0)
+specify('docs', function (a) {
+  a.expect(docs.length * 2)
+  docs.forEach(function (s) {
+    a.ok(re.docs.test(s));
+    var reply = bro.docs(message(s, re.docs))
+    console.log(reply)
+    a.ok(reply)
+  })
+})
+
+bro.on('load', function () {
+  specify.run(filters)
+})
